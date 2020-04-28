f0712a3a12330cbfaa836c9268916f34682173e2
kk <ngkunqi2008@hotmail.com>
2020-04-28 11:21:24 +0800

    First Init

---
+++ client/build/precache-manifest.d45c4227faa722f8f7ca6e44ff3c0b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b4d66ae069aed3cd51e128abfc9918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dcfa6ea08e62b6f8367",
+    "url": "/static/css/main.8ef7ac6e.chunk.css"
+  },
+  {
+    "revision": "384a64d6d9af07f4e622",
+    "url": "/static/js/2.3b470adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b470adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcfa6ea08e62b6f8367",
+    "url": "/static/js/main.6e4814e2.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
